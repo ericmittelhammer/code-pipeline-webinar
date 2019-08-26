413c34ff562cffa532ae97b813a0f5c4d55591d2
Eric Mittelhammer <code@ericmittelhammer.com>
2019-08-26 16:51:03 -0400

    fix testStats path

--- pipelines/jenkins/send-test-results-to-insights.js
+++ pipelines/jenkins/send-test-results-to-insights.js
@@ -1,7 +1,7 @@
 // takes the test results written by mocha and sends them to insights
 
 const sendTestResultstoInsigts = require('../lib/send-test-results-to-insights');
-const testStats = require('../testStats');
+const testStats = require('../../testStats');
 
 const newRelicAccountId = process.env['NEW_RELIC_ACCOUNT_ID'];
 const newRelicInsertKey = process.env['NEW_RELIC_INSERT_KEY'];
